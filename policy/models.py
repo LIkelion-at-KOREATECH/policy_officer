1643e5c6206fe1ea844c0ba682525b46d084fac0
ChaeChae0505 <chaey@koreatech.ac.kr>
2020-09-16 17:39:25 +0900

    admin 성공 img 사이즈 수정

--- policy/models.py
+++ policy/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-
+#chae
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+#chae
 
 # CATEGORY_CHOICES = (
 #     ("1", "EDUCATION"),
@@ -10,11 +13,12 @@ from django.db import models
 # Create your models here.
 
 class Policies(models.Model): # 모델명의 첫 글자는 무조건 대문자
+    
     image = models.ImageField(upload_to = 'images/')
     name = models.CharField(max_length = 50)
     part = models.CharField(max_length = 255)
     description = models.TextField()
     category = models.CharField(max_length= 50)
-
+ 
     def __str__(self):
         return self.name
--- policy/views.py
+++ policy/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Policies
 
+#chae
 # Create your views here.
 def home(request):
     return render (request, 'home.html')
