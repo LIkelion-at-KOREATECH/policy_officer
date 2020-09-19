2dc2b497e1c36827a6dda2553c1c1e6036204003
whitepurple <qazplook@naver.com>
2020-09-19 18:12:58 +0900

    category table

--- policy/admin.py
+++ policy/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Policies
+from .models import Policies, Category
 
 
 # Register your models here.
 
 admin.site.register(Policies)
+admin.site.register(Category)
